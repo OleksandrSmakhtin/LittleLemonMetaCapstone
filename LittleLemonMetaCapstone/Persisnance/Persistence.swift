7680a49efcb509703d04bd561124b122fbe71e16
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-07-01 18:57:25 +0100

    - Add & handle views

--- LittleLemonMetaCapstone/Persisnance/Persistence.swift
+++ LittleLemonMetaCapstone/Persisnance/Persistence.swift
@@ -10,24 +10,24 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Dish(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
+//    static var preview: PersistenceController = {
+//        let result = PersistenceController(inMemory: true)
+//        let viewContext = result.container.viewContext
+//        for _ in 0..<10 {
+//            let newItem = Dish(context: viewContext)
+//            newItem.timestamp = Date()
+//        }
+//        do {
+//            try viewContext.save()
+//        } catch {
+//            let nsError = error as NSError
+//            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//        }
+//        return result
+//    }()
 
+    
     let container: NSPersistentContainer
-
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "LittleLemonMetaCapstone")
         if inMemory {
@@ -39,5 +39,22 @@ struct PersistenceController {
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func clear() {
+        // Delete all dishes from the store
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Dish")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        let _ = try? container.persistentStoreCoordinator.execute(deleteRequest, with: container.viewContext)
+    }
+    
+    static func oneDish() -> Dish {
+        let dish = Dish(context: shared.container.viewContext)
+        dish.title = "Greek Salad"
+        dish.descriptionDish = "The famous greek salad of crispy lettuce, peppers, olives, our Chicago."
+        dish.price = "10"
+        dish.category = "starters"
+        dish.image = "https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/greekSalad.jpg?raw=true"
+        return dish
     }
 }
---
+++ LittleLemonMetaCapstone/Views/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  LittleLemonMetaCapstone
+//
+//  Created by Oleksandr Smakhtin on 01/07/2023.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
